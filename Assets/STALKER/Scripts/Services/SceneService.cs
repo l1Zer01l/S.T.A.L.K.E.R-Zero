bbb8f4159d1836fe371a3f699cce0eed786ae910
Hard_Code <zero-0_0@bk.ru>
2024-07-05 19:40:11 +0500

    added SceneService

---
+++ Assets/STALKER/Scripts/Services/SceneService.cs
@@ -0,0 +1,33 @@
+﻿/**************************************************************************\
+   Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace StalkerZero.Services
+{
+    public class SceneService
+    {
+        public event UnityAction<Scene, LoadSceneMode> LoadSceneEvent 
+        { 
+            add => SceneManager.sceneLoaded += value;
+            remove => SceneManager.sceneLoaded -= value;                                                       
+        }
+
+        public SceneService() 
+        {
+            
+        }
+
+        public string GetCurrentSceneName()
+        {
+            return SceneManager.GetActiveScene().name;
+        }
+
+        public void LoadMenu()
+        {
+
+        }
+    }
+}
