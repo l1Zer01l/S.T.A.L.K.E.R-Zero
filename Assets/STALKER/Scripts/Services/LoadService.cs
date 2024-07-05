ddbcb469e9a7ea1901f5de7976332a785a234668
Hard_Code <zero-0_0@bk.ru>
2024-07-05 19:40:47 +0500

    added LoadService

---
+++ Assets/STALKER/Scripts/Services/LoadService.cs
@@ -0,0 +1,28 @@
+﻿/**************************************************************************\
+   Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using UnityEngine;
+
+namespace StalkerZero.Services
+{
+    public class LoadService
+    {
+        public const string PREFAB_UIROOT = "UI/UIRoot";
+        public LoadService()
+        {
+
+        }
+
+        public T LoadPrefab<T>(string path) where T : Object
+        {
+            var prefab = Resources.Load<T>(path);
+            if (prefab is null)
+            {
+                Debug.LogError($"Can't load prefab from: {path}");
+                throw new System.MethodAccessException();
+            }
+            return prefab;
+        }
+    }
+}
